3cd02f1e3d2d29fb39e7ccaddc4aa1c9a4fd3bdc
Anton Savoskin <anton@sliday.com>
2017-02-17 13:04:55 +0700

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import 'react-select/dist/react-select.css';
+
+render(<div>Place your application here</div>, document.getElementById('app-root'));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/app.jsx')
+  ],
+  resolve: {
+    root: [
+      path.resolve(__dirname, "src"),
+    ],
+    extensions: ['', '.js', '.jsx', '.css']
+  },
+  output: {
+    path: path.join(__dirname, '/public/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  }
+};
